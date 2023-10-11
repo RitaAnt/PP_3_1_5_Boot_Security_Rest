1ecd95aab0332145dc694dd7ed449990a44827e2
Rita M <kolotvinova03@bk.ru>
2023-10-11 03:17:40 +0300

    controllers and entities

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -0,0 +1,34 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.service.AdminService;
+
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public UserController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/user")
+    public String showUser(Model model, Principal principal) {
+        User user = adminService.getUserByName(principal.getName());
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        model.addAttribute("user", user);
+        return "user";
+    }
+}
