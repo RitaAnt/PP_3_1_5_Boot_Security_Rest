1697cd747303ccbf23c4b8bb613abd6ee6beae35
Rita M <kolotvinova03@bk.ru>
2023-11-26 06:31:13 +0300

    add js (not work)

--- src/main/resources/static/js/new.js
+++ src/main/resources/static/js/new.js
@@ -1,28 +1,26 @@
-//Не показывает роли
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/api/roles")
+        .then(response => response.json())
+        .then(roles => {
+            var rolesContainer = document.getElementById('rolesContainer');
+            roles.forEach(role => {
+                var checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.name = 'roles';
+                checkbox.id = 'roleCheckbox' + role.id;
+                checkbox.value = role.id;
+                checkbox.classList.add('form-check-input');
 
+                var label = document.createElement('label');
+                label.htmlFor = 'roleCheckbox' + role.id;
+                label.classList.add('form-check-label');
+                label.innerText = role.name;
 
-var csrfToken = document.querySelector('meta[name="_csrf"]').content;
-document.getElementById('csrfToken').value = csrfToken;
-
-
-var rolesContainer = document.getElementById('rolesContainer');
-var listRoles = [];
-
-listRoles.forEach(function (role) {
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.name = 'roles';
-    checkbox.id = 'roleCheckbox' + role.id;
-    checkbox.value = role.id;
-    checkbox.classList.add('form-check-input');
-
-    var label = document.createElement('label');
-    label.htmlFor = 'roleCheckbox' + role.id;
-    label.classList.add('form-check-label');
-    label.innerText = role.name;
-
-    rolesContainer.appendChild(checkbox);
-    rolesContainer.appendChild(label);
+                rolesContainer.appendChild(checkbox);
+                rolesContainer.appendChild(label);
+            });
+        })
+        .catch(error => console.error("Error fetching roles:", error));
 });
 
 function submitForm() {
