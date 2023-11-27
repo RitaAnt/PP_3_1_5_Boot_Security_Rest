d16966ea7b9460bd0cc9c0669cd7bb42fa13e907
Rita M <kolotvinova03@bk.ru>
2023-11-27 06:20:11 +0300

    delete sort

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -2,7 +2,6 @@ package ru.kata.spring.boot_security.demo.service;
 
 
 
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.entities.Role;
 import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
