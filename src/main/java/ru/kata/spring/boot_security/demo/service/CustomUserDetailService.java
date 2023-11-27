4d9188e45f8e17d08a5399020fb6c141ced3ec8e
Rita M <kolotvinova03@bk.ru>
2023-11-27 06:28:17 +0300

    изменения от ментора interface RoleRepository , interface UserRepository - удали @Repository
    class CustomUserDetailService
    - метод loadUserByUsername() не нуждается в транзакции - напиши запрос использующий join fetch что бы сразу получить пользователя с ролями. Тут можно вернуть юсера а не юсерДетаилз

--- src/main/java/ru/kata/spring/boot_security/demo/service/CustomUserDetailService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/CustomUserDetailService.java
@@ -1,42 +1,29 @@
 package ru.kata.spring.boot_security.demo.service;
 
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.entities.Role;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 
 @Service
 public class CustomUserDetailService implements UserDetailsService {
-    private final UserRepository userRepository;
 
+    private final UserRepository userRepository;
 
     public CustomUserDetailService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameAndRoles(username);
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                mapRolesToAuthorities(user.getRoles()));
+        return user;
     }
-
-    private List<? extends GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
-        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
-    }
-
 
 }
